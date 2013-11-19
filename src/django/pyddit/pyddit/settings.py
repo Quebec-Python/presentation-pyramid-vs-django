dcd8dbc2a1a55a7ed37fc291858bf8c888b6e064
bchhun <bernard.chhun@gmail.com>
2013-11-18 22:32:05 -0500

    modif du host de la BD + ajout des urls de l'app

--- src/django/pyddit/pyddit/settings.py
+++ src/django/pyddit/pyddit/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
         'NAME': 'django-db',
         'USER': 'django',
         'PASSWORD': 'django',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3333'
     }
 }
--- src/django/pyddit/pyddit/urls.py
+++ src/django/pyddit/pyddit/urls.py
@@ -1,12 +1,17 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pyddit.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('pyddit_app.views',
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'home', name="home"),
+    url(r'^vote_up/(?P<hash_url>.+)$', 'vote_up', name="vote_up"),
+    url(r'^vote_down/(?P<hash_url>.+)$', 'vote_down', name="vote_down"),
+    url(r'^add_post$', 'add_post', name="add_post"),
+    url(r'^post/(?P<hash_url>.+)$', 'get_post', name="get_post"),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
