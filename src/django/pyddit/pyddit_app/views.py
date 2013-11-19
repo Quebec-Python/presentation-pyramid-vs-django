96b77b18713d59b867736914cca2c35d041652b5
bchhun <bernard.chhun@gmail.com>
2013-11-18 22:47:11 -0500

    finalisation de l'app django

--- src/django/pyddit/pyddit_app/views.py
+++ src/django/pyddit/pyddit_app/views.py
@@ -1,21 +1,60 @@
 from django.shortcuts import render, redirect
 
 from pyddit_app.forms import PostForm
+from pyddit_app.models import Post
+from pyddit_app.helpers import create_hashed_url
 
 def home(request):
-    context = {'hide_add_button': False}
+    context = {
+        'hide_add_button': False,
+        "posts": Post.objects.all().order_by("-votes")
+    }
     return render(request, 'index.html', context)
 
 def add_post(request):
-    context = dict(hide_add_button=True, form=PostForm())
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            url = form.cleaned_data['url']
+
+            post = Post(
+                title = title,
+                hash_url = create_hashed_url(title),
+                url = url
+            )
+            post.save()
+
+            return redirect("home")
+    else:
+        form = PostForm()
+
+    context = dict(hide_add_button=True, form=form)
+
     return render(request, 'add_post.html', context)
 
 def get_post(request, hash_url):
-    context = dict(hide_add_button=True)
+    context = dict(
+        hide_add_button = True,
+        post = Post.objects.get(hash_url=hash_url)
+    )
     return render(request, 'get_post.html', context)
 
 def vote_up(request, hash_url):
+    post = Post.objects.get(hash_url=hash_url)
+
+    if post:
+        post.votes += 1
+        post.save()
+
     return redirect("home")
 
 def vote_down(request, hash_url):
+    post = Post.objects.get(hash_url=hash_url)
+
+    if post:
+        post.votes -= 1
+        post.save()
+
     return redirect("home")
